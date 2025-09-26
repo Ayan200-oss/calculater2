5db87558aa9ee96878eb17da86e74ab9e7755e87
Ayan200-oss <ayansalman601@gmail.com>
2025-09-26 11:18:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+let string = "";
+
+function display(num) {
+    string = string + num;
+    document.getElementById("input-field").value = string;
+}
+
+function equal() {
+    try {
+        string = eval(string);
+        document.getElementById("input-field").value = string;
+    } catch (e) {
+        document.getElementById("input-field").value = "Error";
+        string = "";
+    }
+}
+
+function clr() {
+    string = "";
+    document.getElementById("input-field").value = string;
+}
+
+function del() {
+    string = string.slice(0, -1);
+    document.getElementById("input-field").value = string;
+}
+function percent() {
+    try {
+        string = (eval(string) / 100).toString();
+        document.getElementById("input-field").value = string;
+    } catch (e) {
+        document.getElementById("input-field").value = "Error";
+        string = "";
+    }
+}
+//     string=string+num;
+//     document.getElementById("result").value=string;
+// }
+// function equal(){
+//     string=eval(string);
+//     document.getElementById("result").value=string;
+// }
+// function clr(){
+//     string="";
+//     document.getElementById("result").value=string;
+// }
+// function del(){
+//     string=string.slice(0,-1);
+//     document.getElementById("result").value=string;
+// }
+// function num(){
+//     alert("hi");
+// }   
